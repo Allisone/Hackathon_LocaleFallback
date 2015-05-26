88409ac8ce61d43d5fea69f9029d96d66d626db5
Sebastian Wojtowicz <trust.sebastian@gmail.com>
2015-05-26 09:00:32 +0200

    [TASK] overwriting getTemplateFile method to have a fallback there too
    If a template file doesn't exist we now try the fallback.
    If the fallback doesn't exist we use default locale.
    If the default locale doesn't exist we use en_US.

--- app/code/community/Hackathon/LocaleFallback/Model/Translate.php
+++ app/code/community/Hackathon/LocaleFallback/Model/Translate.php
@@ -160,4 +160,45 @@ class Hackathon_LocaleFallback_Model_Translate extends Mage_Core_Model_Translate
 
         return $this->getData();
     }
+
+    /**
+     * Retrive translated template file
+     *
+     * @param string $file
+     * @param string $type
+     * @param string $localeCode
+     * @return string
+     */
+    public function getTemplateFile($file, $type, $localeCode=null)
+    {
+        if (is_null($localeCode) || preg_match('/[^a-zA-Z_]/', $localeCode)) {
+            $localeCode = $this->getLocale();
+        }
+
+        $filePath = Mage::getBaseDir('locale')  . DS
+            . $localeCode . DS . 'template' . DS . $type . DS . $file;
+
+        if (!file_exists($filePath)) { // If template doesn't exist, use fallback
+            $filePath = Mage::getBaseDir('locale') . DS
+                . Mage::getStoreConfig('general/locale/code_fallback')
+                . DS . 'template' . DS . $type . DS . $file;
+        }
+
+        if (!file_exists($filePath)) { // If fallback doesn't exist, use store default
+            $filePath = Mage::getBaseDir('locale') . DS
+                . Mage::app()->getLocale()->getDefaultLocale()
+                . DS . 'template' . DS . $type . DS . $file;
+        }
+
+        if (!file_exists($filePath)) {  // If store default doesn't exist, use en_US
+            $filePath = Mage::getBaseDir('locale') . DS
+                . Mage_Core_Model_Locale::DEFAULT_LOCALE
+                . DS . 'template' . DS . $type . DS . $file;
+        }
+
+        $ioAdapter = new Varien_Io_File();
+        $ioAdapter->open(array('path' => Mage::getBaseDir('locale')));
+
+        return (string) $ioAdapter->read($filePath);
+    }
 }
